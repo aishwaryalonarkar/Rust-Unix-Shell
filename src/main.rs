54db1551fe446721a055563a2a3c5e5ed791292e
ov-wagle <ov.wagle@gmail.com>
2022-02-23 15:33:12 -0500

    Remove test calls to history function and add shell handling

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ mod util;
 
 fn main() {
     let mut history = util::initialize_vector();
-    history = util::add_command_to_history(history, String::from("ls"));
-    history = util::add_command_to_history(history, String::from("cd .."));
-    history = util::add_command_to_history(history, String::from("mkdir"));
-    history = util::add_command_to_history(history, String::from("touch abc.txt"));
-    history = util::list_history(history);
+
+    loop {
+        print!("rustshell@rustshell:$ ");
+    }
+    
 }
 
 
