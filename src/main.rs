2115b5b47d8792a1f604f37b3655560722f05f98
ov-wagle <ov.wagle@gmail.com>
2022-02-23 15:23:32 -0500

    Add basic functions to add command to history

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod util;
 
 fn main() {
-    println!("Hello, world!");
+    let mut history = util::initialize_vector();
+    history = util::add_command_to_history(history, String::from("ls"));
+    history = util::add_command_to_history(history, String::from("cd .."));
+    history = util::add_command_to_history(history, String::from("mkdir"));
+    history = util::add_command_to_history(history, String::from("touch abc.txt"));
+    history = util::list_history(history);
 }
+
+
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,22 @@
-pub struct DoublyLinkedList<'a> {
-    command: String,
-    next: &'a DoublyLinkedList<'a>,
-    prev: &'a DoublyLinkedList<'a>
+// Initialize the vector to store the list of commands entered on the shell
+pub fn initialize_vector() -> Vec<String> {
+    let vec = Vec::new();
+
+    vec
+}
+
+// Add command to the history vector
+pub fn add_command_to_history(mut history:Vec<String>, command:String) -> Vec<String> {
+    history.push(command);
+
+    history
+}
+
+// Print the commands present in the history vector
+pub fn list_history(history:Vec<String>) -> Vec<String> {
+    for i in 0..history.len() {
+        println!("{}", history[i]);
+    }
+
+    history
 }
