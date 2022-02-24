be64d0376621d03a1adc3fc0e2caa5df8d22ea5d
ov-wagle <ov.wagle@gmail.com>
2022-02-24 15:19:46 -0500

    Implement history command

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-#[macro_use]
-extern crate scan_rules;
 mod util;
+use std::io::Write;
+use std::cmp::Ordering;
 
 fn main() {
     let mut history = util::initialize_vector();
+    let command_history:String = String::from("history");
+    let mut command:String = String::new();
 
     loop {
         print!("rustshell@rustshell:$ ");
-        let result = try_readln! {
-            (let command: String) => (command)
-        };
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut command).unwrap();
+
+        history = util::add_command_to_history(history, command.clone());
+        
+        //println!("{}", command);
+        if command.eq(&command_history) {
+            history = util::list_history(history);
+        } else {
+            println!("Invalid command");
+        }
+        command.clear();
     }
     
 }
